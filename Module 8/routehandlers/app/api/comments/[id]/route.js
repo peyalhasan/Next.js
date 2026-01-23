48e3074ec1521dcbaab697e3cce88201d1c2719f
Peyal Hasan <peyalhasan143@gmail.com>
2026-01-23 18:03:56 +0600

    Headers and Cookies Fri Jan 23 18:03:56 BST 2026

--- Module 8/routehandlers/app/api/comments/[id]/route.js
+++ Module 8/routehandlers/app/api/comments/[id]/route.js
@@ -1,39 +1,18 @@
 import { comments } from "@/app/data/comments";
-
-// export async function GET(request, { params }) {
-//     const { id } = params;
-
-//     const comment = comments.find(comment => comment.id === parseInt(id))
-
-//     return Response.json(comment)
-// }
-
-// export async function PATCH(request, { params }) {
-//     const { id } = params;
-
-//     const commentIndex = comments.findIndex(comment => comment.id === parseInt(id));
-
-//     const newComment = await request.json();
-//     comments[commentIndex].text = newComment.text;
-
-//     return Response.json(comments[commentIndex])
-// }
-
-// export async function DELETE(_request,{ params }) {
-//     const { id } = params;
-//     const commentIndex = comments.findIndex(comment => comment.id === parseInt(id));
-
-//     const commentToDelete = comments[commentIndex];
-//     comments.splice(commentIndex, 1);
-
-//     return Response.json(commentToDelete)
-// }
+import { redirect } from "next/navigation";
 
 
-export async function GET(_request, { params }) {
+
+
+
+export async function GET(request, { params }) {
     const { id } = params;
 
-    const comment = comments.find(comment => comment.id === parseInt(id))
+
+    if (parseInt(id) > comments.length){
+        redirect('/api/comments')
+    }
+        const comment = comments.find(comment => comment.id === parseInt(id))
 
     return Response.json(comment)
 }
--- Module 8/routehandlers/app/api/comments/route.js
+++ Module 8/routehandlers/app/api/comments/route.js
@@ -1,20 +1,27 @@
 import { comments } from "@/app/data/comments"
-export function GET() {
+export function GET(request) {
+    const searchParams = request.nextUrl.searchParams;
+    const queary = searchParams.get('query');
+    if (queary) {
+        const filteredQuery = comments.filter(comment => comment.text.toLocaleLowerCase().includes(queary))
+
+        return Response.json(filteredQuery)
+    }
     return Response.json(comments)
 }
 
 export async function POST(request) {
-   const comment = await request.json();
-   const newComment = {
-    id: comments.length + 1,
-    text: comment.text,
-   };
-   comments.push(newComment);
+    const comment = await request.json();
+    const newComment = {
+        id: comments.length + 1,
+        text: comment.text,
+    };
+    comments.push(newComment);
 
-   return new Response(JSON.stringify(newComment),{
-    headers: {
-        'Content-Type': 'comments/json'
-    },
-    status: 201
-   })
+    return new Response(JSON.stringify(newComment), {
+        headers: {
+            'Content-Type': 'comments/json'
+        },
+        status: 201
+    })
 }
